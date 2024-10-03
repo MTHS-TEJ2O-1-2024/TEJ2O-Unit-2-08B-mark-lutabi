f65b9a14280355490eaa79094757688607cbe0c7
mark-lutabi <mark.lutabi@mths.ca>
2024-10-03 11:00:13 -0400

    RGB color change code

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark lutabi
+ * Created on: Sep 2024
+ * This program shows different colors for RGB LED
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+
+  
+
+
+
+
+})
